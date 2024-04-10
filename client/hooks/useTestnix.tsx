5fdc9a3381858cc8317c19043c1d2114aa417a55
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-04-10 13:33:33 +0530

    commit

--- client/hooks/useTestnix.tsx
+++ client/hooks/useTestnix.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 
 const useTestnix = () => {
     const trackClicks = async ({ hash, experimentId, variantId }: { hash: string, experimentId: string, variantId: string }) => {
-        await fetch(`https://testnix.vercel.app/api/v1/event-clicks`, {
+        const fetchedVariant = await fetch(`https://testnix.vercel.app/api/v1/event-clicks`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({ hash, variantId, experimentId }),
         });
-        // const fetchedData = await fetchedVariant.json();
-        // if (fetchedData.success == false) {
-        //     throw new Error(fetchedData.error);
-        // }
+        const fetchedData = await fetchedVariant.json();
+        if (fetchedData.success == false) {
+            throw new Error(fetchedData.error);
+        }
     };
 
     const trackConversions = async ({ hash, experimentId, variantId }: { hash: string, experimentId: string, variantId: string }) => {
